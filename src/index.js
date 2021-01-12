f84341b8717878ac2ae1e818f4dbac4152a4c007
proudpdd <proud_panda@hotmail.com>
2021-01-12 22:42:10 +0700

    data

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import axios from "axios";
+import jwt from "jsonwebtoken";
 // import database from "./database";
 // import User from "./model/User";
 
@@ -20,16 +21,23 @@ app.use(bodyParser.json());
 const validateAuthentication = async (req, res, next) => {
   const json = JSON.stringify(req.body);
   try {
-    const response = await axios.post('https://myapi.ku.th/auth/login', json, {
+    const response = await axios.post("https://myapi.ku.th/auth/login", json, {
       headers: {
-        'Content-Type': 'application/json',
-        'app-key' : 'txCR5732xYYWDGdd49M3R19o1OVwdRFc'
+        "Content-Type": "application/json",
+        "app-key": "txCR5732xYYWDGdd49M3R19o1OVwdRFc",
       },
-    })
-    res.status(200).send({ data: response.data });
-    } catch (error) {
-      res.status(500).send({"code":"ldap_error","message":"Account "+req.body.username+" not found.","user":null});
-    }
+    });
+    res.status(200).send({
+      "x-access-token": response.data.accesstoken,
+    });
+    console.log(response.data);
+  } catch (error) {
+    res.status(500).send({
+      code: "ldap_error",
+      message: "Account " + req.body.username + " not found.",
+      user: null,
+    });
+  }
   // const path = 'https://myapi.ku.th/std-profile/checkGrades?idcode='.concat(response.data.user.idCode)
   // const userData = await axios.get(path, {
   //   headers: {
@@ -40,12 +48,43 @@ const validateAuthentication = async (req, res, next) => {
   //   })
   // res.status(200).send({ data: response.data });
   // next()
-}
+};
+
+const decodeToken = async (req, res, next) => {
+  const token = req.headers.authorization.split(" ")[1];
+  const decoded = jwt.decode(token);
+  req.user = decoded;
+  next();
+};
 
 app.post("/login", validateAuthentication, async (req, res) => {
   res.status(200).send({ message: "ok" });
 });
 
+app.post("/detail", decodeToken, async (req, res) => {
+  // console.log(req.headers.authorization.split(" ")[1]);
+  // const json = JSON.stringify(req.body);
+  // console.log(req.user);
+  try {
+    const detail = await axios.get(
+      `https://myapi.ku.th/std-profile/getStdEducation?stdId=`.concat(
+        req.user.stdid
+      ),
+      {
+        headers: {
+          "Content-Type": "application/json",
+          "app-key": "txCR5732xYYWDGdd49M3R19o1OVwdRFc",
+          "x-access-token": req.headers.authorization.split(" ")[1],
+        },
+      }
+    );
+    // console.log(detail.data.results.education);
+    res.status(200).send({ data: detail.data, baseDetail: req.user });
+  } catch (error) {
+    res.status(500).send("error");
+  }
+  // res.send("hello");
+});
 // app.get("/login", async (req, res) => {
 //   res.status(401).send('please');
 // });
